b9ca276bbc5d8c75091d1ab9c135216755be75c3
jpaulocts <jpaulocts@gmail.com>
2024-02-18 00:28:07 -0300

    updating project

--- dist/assets/cart-C4E8G0bN.js
+++ dist/assets/cart-C4E8G0bN.js
@@ -1,4 +1,4 @@
-import{g as i,t as l,l as p}from"./utils-DS4vTEgP.js";import{i as d}from"./ProductList-BJMuNnwB.js";function m(r){const e=d(r);return`<li class="cart-card divider">
+import{g as i,t as l,l as p}from"./utils-CWMTnRNg.js";import{i as d}from"./ProductList-YyOqZC3o.js";function m(r){const e=d(r);return`<li class="cart-card divider">
   <a href="/recipe_page/index.html?recipe=${e}" class="cart-card__image">
     <img
       src="${r.recipe.image}"
---
+++ dist/assets/listing-K9tLo4yX.js
@@ -0,0 +1,1 @@
+import{l as e,b as t}from"./utils-CWMTnRNg.js";import{E as o}from"./ExternalServices-Dv5AZ21x.js";import{P as r}from"./ProductList-YyOqZC3o.js";e();const s=t("mealType"),a=new o,c=document.querySelector(".product-list"),m=new r(s,a,c);m.init();
